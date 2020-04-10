e36080fa0d756447e30633eae278a3b9e7f35cae
Quoll42 <Quoll42@users.noreply.github.com>
2020-04-10 22:28:13 +0800

    Updates

---
+++ precache-manifest.7b4101d9887605c0c814bb023b7fd1c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c2f791199d887e7ed8d69a87ecb208",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "6ae1443c8dde94dab18f",
+    "url": "/react-tutorial/static/css/main.4e33fe1a.chunk.css"
+  },
+  {
+    "revision": "607c18d14458d18821eb",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae1443c8dde94dab18f",
+    "url": "/react-tutorial/static/js/main.6738c698.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
